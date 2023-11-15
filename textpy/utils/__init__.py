251d65bebf32d7fb2ea00bdc25df430e41bf3b2f
Chitaoji <2360742040@qq.com>
2023-11-15 08:50:52 +0800

    Reducing initializing jobs

--- textpy/utils/__init__.py
+++ textpy/utils/__init__.py
@@ -1,16 +1,3 @@
-import warnings
+from . import re_extended
 
 __all__ = []
-
-# import `re_extended` if exists
-try:
-    from . import re_extended
-
-    __all__.extend(re_extended.__all__)
-    from .re_extended import *
-
-except ImportError as e:
-    if isinstance(e, ModuleNotFoundError):
-        raise e
-    else:
-        warnings.warn(e.msg, Warning)
