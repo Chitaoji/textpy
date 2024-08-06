ce7d9908d1f286233d09fc71dcf72a39b355e17c
yifanpan <2360742040@qq.com>
2024-08-06 16:05:11 +0800

    New method Imports.groupby()

--- src/imports.py
+++ src/imports.py
@@ -7,7 +7,8 @@ NOTE: this module is private. All functions and objects are available in the mai
 """
 
 import re
-from typing import TYPE_CHECKING, List, NamedTuple, Optional
+from functools import cached_property
+from typing import TYPE_CHECKING, Any, Dict, List, Literal, NamedTuple, Optional, Union
 
 from typing_extensions import Self
 
@@ -15,6 +16,9 @@ if TYPE_CHECKING:
     from .abc import PyText
 
 __all__ = []
+
+HistoryKey = Literal["where", "fro", "name", "as_name", "type_checking"]
+HistoryGroupResult = Dict[Any, Union["HistoryGroupResult", List["ImportHistory"]]]
 
 
 class ImportHistory(NamedTuple):
@@ -57,28 +61,88 @@ class Imports:
 
     def __init__(self, obj: "PyText") -> None:
         self.pytext_obj = obj
-        if obj.is_dir():
-            self.children = [Imports(x) for x in obj.children]
-            self.history = []
-            for c in self.children:
-                self.history.extend(c.history)
-        else:  # obj.is_file()
-            self.children = []
-            self.history = self.__get_history()
 
-    def __get_history(self) -> List[ImportHistory]:
+    @cached_property
+    def children(self) -> List[Self]:
+        """Children nodes."""
+        if self.pytext_obj.is_dir():
+            return list(map(Imports, self.pytext_obj.children))
+        return []
+
+    @cached_property
+    def history(self) -> List[ImportHistory]:
+        """Import history."""
+        if self.children:
+            hist = []
+            for c in self.children:
+                hist.extend(c.history)
+            return hist
         pattern = (
             "(?: *from +)?(?:([.\\w]+) )?(?: *import +)"
             "((?:[.\\w]+(?: +as +)?(?:[.\\w]+)? *,? *)+)"
         )
-        hist: List = []
-        text = re.sub(
-            "\nif +TYPE_CHECKING *:(\n+    .*)+(\nelse *:)?", "", self.pytext_obj.text
-        )
+        type_check_pattern = "(?:\n|^)if +TYPE_CHECKING *:(?:\n+    .*)+(?:\nelse *:)?"
+        text = re.sub(type_check_pattern, "", self.pytext_obj.text)
+        type_check_text = "".join(re.findall(type_check_pattern, self.pytext_obj.text))
+        hist = self.__text2hist(pattern, text, False)
+        hist.extend(self.__text2hist(pattern, type_check_text, True))
+        return hist
+
+    def __text2hist(
+        self, pattern: str, text: str, type_checking: bool
+    ) -> List[ImportHistory]:
+        hist = []
         for line in text.splitlines():
             if matched := re.match(pattern, line):
                 fro, imported = matched.groups()
-                hist.append(
-                    ImportHistory(self.pytext_obj.absname, fro, name, as_name, False)
-                )
+                for names in re.split(" *, *", imported):
+                    n, a = re.match("([.\\w]+)(?: +as +)?([.\\w]+)?", names).groups()
+                    hist.append(
+                        ImportHistory(self.pytext_obj.absname, fro, n, a, type_checking)
+                    )
         return hist
+
+    def groupby(self, *by: Union[HistoryKey, List[HistoryKey]]) -> HistoryGroupResult:
+        """
+        Group the import history by the key.
+
+        Parameters
+        ----------
+        *by : Union[str, List[str]]
+            Used to determine the groups for the groupby.
+
+        Returns
+        -------
+        Dict[Union[str, Tuple[str]], ...]
+            Group results.
+
+        """
+        if len(by) >= 1:
+            groups = self.__hitory_groupby(self.history, by[0])
+            for key in by[1:]:
+                groups = self.__hitory_groupby(groups, key)
+            return groups
+        raise ValueError("'groupby()' accepts at least 1 argument")
+
+    @staticmethod
+    def __hitory_groupby(
+        history: Any, by: Union[HistoryKey, List[HistoryKey]]
+    ) -> HistoryGroupResult:
+        if isinstance(history, dict):
+            return {k: Imports.__hitory_groupby(v, by) for k, v in history.items()}
+
+        groups: Dict[Any, List[ImportHistory]] = {}
+        if isinstance(by, str):
+            key = ImportHistory._fields.index(by)
+            for h in history:
+                if (v := h[key]) not in groups:
+                    groups[v] = []
+                groups[v].append(h)
+        else:
+            keys = tuple(map(ImportHistory._fields.index, by))
+            for h in history:
+                values = tuple(h[k] for k in keys)
+                if values not in groups:
+                    groups[values] = []
+                groups[values].append(h)
+        return groups
